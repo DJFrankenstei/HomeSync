7a6fabe9f63a6dd57ca45b2dd24c4c4ba7bdded0
OneLeaf-33013 <apexgp01@gmail.com>
2026-03-23 14:04:58 +0530

    Initial Commit

---
+++ HomeSync/HomeSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSyncApp.swift
+//  HomeSync
+//
+//  Created by OneLeaf on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
